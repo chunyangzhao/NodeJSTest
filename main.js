02d3fadc67382318daf88a66301cc9ff1bc48d36
zcy <291842911@qq.com>
2024-08-05 15:12:55 +0800

    add auto shutdown code

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 mode = process.argv[2]
 console.log("Hello world", mode)
+
+// windows自动执行脚本后关机
+// @echo off  
+// set NODE_SKIP_PLATFORM_CHECK=1
+// :: 设置Node.js程序的路径  
+// set NODE_PROGRAM="D:\CodeProjects\NodeJSTest\main.js" 
+  
+// :: 执行Node.js程序  
+// node %NODE_PROGRAM% start
+
+// pause
+// 关机
+// shutdown /s /t 60
