f2914b4ca88ecb4990413da42fde01e4d29df6e1
zcy <291842911@qq.com>
2024-08-06 17:48:02 +0800

    add rename

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 mode = process.argv[2]
 console.log("Hello world", mode)
 
+
+// ren F_config.js my_config.js
+// ren F_env.js env.js
+// set NODE_PROGRAM="D:\CodeProjects\NodeJSTest\main.js" 
+
 // windows自动执行脚本后关机
 // @echo off  
 // set NODE_SKIP_PLATFORM_CHECK=1
@@ -10,6 +15,8 @@ console.log("Hello world", mode)
 // :: 执行Node.js程序  
 // node %NODE_PROGRAM% start
 
+// ren my_config.js F_config.js 
+// ren env.js F_env.js 
 // pause
 // 关机
 // shutdown /s /t 60
